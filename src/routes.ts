a597f4c0eaf5c1b9336dcb43247efed6d496aa59
Caleb Freitas <calebfreitas@tutanota.com>
2022-02-07 18:17:26 -0300

    chore: add /tags route

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 
+import { CreateTagController } from "./controllers/tags/CreateTagController";
 import { CreateUserController } from "./controllers/users/CreateUserController";
+import { ensureAdmin } from "./middleware/ensureAdmin";
 
 const routes = Router();
 
 const createUserController = new CreateUserController();
+const createTagController = new CreateTagController();
 
 routes.post("/users", createUserController.handle);
+routes.post("/tags", ensureAdmin, createTagController.handle);
 
 export { routes };
