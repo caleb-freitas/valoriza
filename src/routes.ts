b3bbb44b14bd1428ac7a54ea6ef90ba0997f2457
Caleb Freitas <calebfreitas@tutanota.com>
2022-02-08 13:08:24 -0300

    chore: add /login and /compliment routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 
+import { CreateComplimentController } from "./controllers/compliments/CreateComplimentController";
 import { CreateTagController } from "./controllers/tags/CreateTagController";
+import { AuthenticateUserController } from "./controllers/users/AuthenticateUserController";
 import { CreateUserController } from "./controllers/users/CreateUserController";
 import { ensureAdmin } from "./middleware/ensureAdmin";
 
@@ -8,8 +10,12 @@ const routes = Router();
 
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
+const authenticateUserController = new AuthenticateUserController();
+const createComplimentController = new CreateComplimentController();
 
 routes.post("/users", createUserController.handle);
 routes.post("/tags", ensureAdmin, createTagController.handle);
+routes.post("/login", authenticateUserController.handle);
+routes.post("/compliments", createComplimentController.handle);
 
 export { routes };
